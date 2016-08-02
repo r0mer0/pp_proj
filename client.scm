0ed977c7c3abc4884c4f0901f312117158356b8f
Marcos Romero <marcosromero11@gmail.com>
2016-08-01 22:22:08 -0300

    "Feito primeira funcao pow"

--- client.scm
+++ client.scm
@@ -1,10 +1,12 @@
 #lang racket
 ;TCP Client test;
 (require racket/tcp)
-
 (define-values (i o) (tcp-connect "localhost" 5400))
 
 (file-stream-buffer-mode i 'none)
 (file-stream-buffer-mode o 'none)
+(write "connected" o)
 (read i)
-(write "Good Bye" o)
+			
+
+
--- server.scm
+++ server.scm
@@ -1,4 +1,14 @@
 #lang racket
+;Define functions
+(define pow
+	(lambda (x n)
+		(let pow_i ((i 0) (p 1))
+			(cond			
+				((= n i) p)
+				((> n 0) (pow_i (+ i 1) (* p x)))
+				(else (pow_i (- i 1) (/ p x)))))))
+             
+
 ;TCP test server;
 (require racket/tcp)
 (define l (tcp-listen 5400))
@@ -7,9 +17,8 @@
 
 (file-stream-buffer-mode i 'none)
 (file-stream-buffer-mode o 'none)
-
-(write "hi" o)
 (read i)
+(write "Bem-vindo ao UFAlphaBC. Entre com a operacao solicitada:" o)   
 (close-input-port i)
 (close-output-port o)
 
